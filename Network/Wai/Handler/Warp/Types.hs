martin-bailey/wai

Receive buffering performance optimization, no memcpy and less malloc

warp/Network/Wai/Handler/Warp/Types.hs
@@ -12,7 +12,7 @@ import Network.Socket (Socket)
 import qualified Network.Wai.Handler.Warp.Date as D
 import qualified Network.Wai.Handler.Warp.FdCache as F
 import qualified Network.Wai.Handler.Warp.Timeout as T
-import Network.Wai.Handler.Warp.Buffer (Buffer,BufSize)
+import Network.Wai.Handler.Warp.Buffer (Buffer,BufferPool,BufSize)
 import qualified Data.ByteString as S
 import Data.IORef (IORef, readIORef, writeIORef, newIORef)
 
@@ -76,7 +76,9 @@ data Connection = Connection
     , connSendFile :: FilePath -> Integer -> Integer -> IO () -> [ByteString] -> IO () -- ^ filepath, offset, length, hook action, HTTP headers
     , connClose    :: IO ()
     , connRecv     :: IO ByteString
+    , connBufferPool :: BufferPool
     , connReadBuffer       :: Buffer
+    -- ^ NOTE: This is kept just for backwards compatibility, and is no longer used at all.
     , connWriteBuffer      :: Buffer
     , connBufferSize       :: BufSize
     , connSendFileOverride :: ConnSendFileOverride

galenhuntington/plain-warp

Network/Wai/Handler/Warp/Types.hs
@@ -12,7 +12,7 @@ import Data.Typeable (Typeable)
 import Data.Word (Word16)
 import Network.HTTP.Types.Header
 import Network.Socket (Socket)
-import Network.Wai.Handler.Warp.Buffer (Buffer,BufSize)
+import Network.Wai.Handler.Warp.Buffer (Buffer,BufSize,BufferPool)
 import qualified Network.Wai.Handler.Warp.Date as D
 import qualified Network.Wai.Handler.Warp.FdCache as F
 import qualified Network.Wai.Handler.Warp.Timeout as T
@@ -79,7 +79,9 @@ data Connection = Connection
     , connSendFile :: FilePath -> Integer -> Integer -> IO () -> [ByteString] -> IO () -- ^ filepath, offset, length, hook action, HTTP headers
     , connClose    :: IO ()
     , connRecv     :: IO ByteString
+    , connBufferPool :: BufferPool
     , connReadBuffer       :: Buffer
+    -- ^ NOTE: This is kept just for backwards compatibility, and is no longer used at all.
     , connWriteBuffer      :: Buffer
     , connBufferSize       :: BufSize
     , connSendFileOverride :: ConnSendFileOverride